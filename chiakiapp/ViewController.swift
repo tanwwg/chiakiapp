04ef2d91be5e93da894ade2b5534755ab057267e
Tan Thor Jen <tan@wisewizardgames.com>
2022-03-29 13:56:08 +0800

    Initial Commit

---
+++ chiakiapp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  chiakiapp
+//
+//  Created by Tan Thor Jen on 29/3/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
